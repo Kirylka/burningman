f853451daf930784f225c90a2ca7c6d456c2f897
Кирилл <kirylanoshko@gmail.com>
2017-09-11 19:34:47 +0300

    fix index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/src'));
 
 app.get('/', function(request, response) {
 	response.sendfile(__dirname + '/dist/index.html');
-}).configure(function() {
-	app.use('/images', express.static(__dirname + '/src/images'));
-}).listen(port);
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
